d045f579971c4d9168b2adf85d77dae8f487eb18
inshuya <inshuyamuthukumar@gmail.com>
2021-03-04 22:15:21 -0600

    added is_Expense from transactions

--- models/transaction.model.js
+++ models/transaction.model.js
@@ -28,9 +28,9 @@ Transactions.getAll = (userId,month, result) => {
 };
 
 Transactions.getBills = (userId,month, result) => {
-  sql.query(`SELECT t.account_number, transaction_name, category_name, transaction_ts, amount 
+  sql.query(`SELECT t.account_number, transaction_name, category_name, transaction_ts, amount, is_expense 
   FROM transactions t JOIN accounts a ON t.account_number = a.account_number 
-  JOIN (SELECT DISTINCT category_id,category_name,user_id FROM category_goal WHERE is_expense = 1) c ON c.category_id = t.category_id AND c.user_id = a.user_id
+  JOIN (SELECT DISTINCT category_id,category_name,user_id, is_expense  FROM category_goal WHERE is_expense = 1) c ON c.category_id = t.category_id AND c.user_id = a.user_id
   WHERE a.user_id = ${userId} and MONTH('${month}') = MONTH(transaction_ts) AND YEAR('${month}') = YEAR(transaction_ts) AND transaction_ts <= NOW()
   ORDER BY transaction_ts DESC`, (err, res) => {
     if (err) {
@@ -51,9 +51,9 @@ Transactions.getBills = (userId,month, result) => {
 };
 
 Transactions.getIncome = (userId,month, result) => {
-  sql.query(`SELECT t.account_number, transaction_name, category_name, transaction_ts, amount 
+  sql.query(`SELECT t.account_number, transaction_name, category_name, transaction_ts, amount, is_expense 
   FROM transactions t JOIN accounts a ON t.account_number = a.account_number 
-  JOIN (SELECT DISTINCT category_id,category_name,user_id FROM category_goal  WHERE is_expense = 0) c ON c.category_id = t.category_id AND c.user_id = a.user_id
+  JOIN (SELECT DISTINCT category_id,category_name,user_id, is_expense  FROM category_goal  WHERE is_expense = 0) c ON c.category_id = t.category_id AND c.user_id = a.user_id
   WHERE a.user_id = ${userId} and MONTH('${month}') = MONTH(transaction_ts) AND YEAR('${month}') = YEAR(transaction_ts) AND transaction_ts <= NOW()
   ORDER BY transaction_ts DESC`, (err, res) => {
     if (err) {
